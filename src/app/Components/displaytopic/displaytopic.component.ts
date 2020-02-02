61e83fb073e11c780c502acc6a83e954c4b76f24
ajinkyasagar <sagarthakare1@gmail.com>
2020-02-02 17:00:43 +0530

    Merge branch 'master' of https://github.com/ajinkyasagar/TutorialWithAngular4.git

---
+++ src/app/Components/displaytopic/displaytopic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-displaytopic',
+  templateUrl: './displaytopic.component.html',
+  styleUrls: ['./displaytopic.component.css']
+})
+export class DisplaytopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
